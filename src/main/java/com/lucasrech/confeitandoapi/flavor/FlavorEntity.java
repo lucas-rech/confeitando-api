8b8a91803aa5e54a2db1fd39a989bd6582faf0c8
Lucas <lucasrech00@gmail.com>
2025-04-05 00:11:38 -0300

    Criada entidade FlavorEntity

---
+++ src/main/java/com/lucasrech/confeitandoapi/flavor/FlavorEntity.java
@@ -0,0 +1,55 @@
+package com.lucasrech.confeitandoapi.flavor;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FlavorEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 60)
+    private String title;
+
+    @Column(nullable = false, length = 255)
+    private String description;
+
+    @Column(nullable = false)
+    private Double value;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        FlavorEntity that = (FlavorEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(title, that.title) && Objects.equals(description, that.description) && Objects.equals(value, that.value) && Objects.equals(imageUrl, that.imageUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, value, imageUrl);
+    }
+
+    @Override
+    public String toString() {
+        return "FlavorEntity{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", value=" + value +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}

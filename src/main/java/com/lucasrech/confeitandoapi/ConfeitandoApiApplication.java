82c702e99d5423cf9d35cae62408c0938c6eb346
Lucas <lucasrech00@gmail.com>
2025-04-05 00:11:01 -0300

    Initial Commit - Configurando projeto

---
+++ src/main/java/com/lucasrech/confeitandoapi/ConfeitandoApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucasrech.confeitandoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfeitandoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfeitandoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasrech/confeitandoapi/ConfeitandoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasrech.confeitandoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeitandoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

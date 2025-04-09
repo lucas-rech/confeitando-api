538ee7cfc48d81cace3ed88e3759718a724d7b98
Lucas <lucasrech00@gmail.com>
2025-04-09 02:35:18 -0300

    feat: Disponibilizado endpoint para acessar imagens salvas no sistema

---
+++ src/main/java/com/lucasrech/confeitandoapi/config/ResourceHandler.java
@@ -0,0 +1,14 @@
+package com.lucasrech.confeitandoapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceHandler implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/flavors/**")
+                .addResourceLocations("file:src/main/resources/static/flavors/");
+    }
+}

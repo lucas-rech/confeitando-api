6c4675aff6fbbbe82459bb56fe0205c1c38e8f4c
Lucas <lucasrech00@gmail.com>
2025-04-10 16:46:47 -0300

    fix: Adicionado tratamento de erro para IO no handler HTTP

--- src/main/java/com/lucasrech/confeitandoapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/lucasrech/confeitandoapi/exceptions/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.IOException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -20,6 +22,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorDTO> handleImageException(ImageException ex) {
         return ResponseEntity.badRequest().body(
                 new ErrorDTO(ex.getMessage())
+        );
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<ErrorDTO> handleIOException(IOException ex) {
+        return ResponseEntity.internalServerError().body(
+                new ErrorDTO("Internal server error: " + ex.getMessage())
         );
     }
 }
